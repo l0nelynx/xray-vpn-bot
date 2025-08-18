68db8fc04ec46dc5166e6a94c98f1dd751e73557
SpicyCheeze <sinikovdmitriy@gmail.com>
2025-08-18 14:53:52 +0300

    User Agreement & Private Policy is here now

--- app/handlers/base.py
+++ app/handlers/base.py
@@ -1,17 +1,21 @@
+import asyncio
+
 import app.database.requests as rq
 import app.keyboards as kb
-
+import app.locale.lang_ru as ru
 
 from aiogram import F, Router
-from aiogram.types import Message, CallbackQuery
+from aiogram.types import Message, CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.filters import Command
 
 from app.handlers.events import userlist
 from app.handlers.tools import startup_user_dialog, free_sub_handler, subscription_info
-from app.locale.lang_ru import text_help
+# from app.locale.lang_ru import text_help
 from app.settings import secrets
+from app.utils import create_smart_invoice
 
 router = Router()
+lang = eval(f"{secrets.get('language')}")
 
 
 @router.message(Command("start"))  # Start command handler
@@ -20,9 +24,41 @@ async def cmd_start(message: Message):
     await startup_user_dialog(message)
 
 
+@router.callback_query(F.data == 'Agreement')  # Start command handler
+async def user_agreement(callback: CallbackQuery):
+    await callback.message.edit_text(text=lang.user_agreement, parse_mode='HTML',
+                                     reply_markup=kb.to_main)
+
+
+@router.callback_query(F.data == 'Privacy')  # Start command handler
+async def user_agreement(callback: CallbackQuery):
+    await callback.message.edit_text(text=lang.privacy_policy, parse_mode='HTML',
+                                     reply_markup=kb.to_main)
+
+
+# @router.message(Command("pay"))  # Start command handler
+# async def cmd_buy(message: Message):
+#     await message.answer(
+#         "Интеграция платежной системы",
+#         reply_markup=InlineKeyboardMarkup(
+#             inline_keyboard=[
+#                 [InlineKeyboardButton(text="КУПИТЬ", callback_data="payment_test")]
+#             ]
+#         )
+#     )
+
+
+@router.callback_query(F.data == 'payment_test')  # Start command handler
+async def payment_test(callback: CallbackQuery):
+    print("Callback init")
+    link = await create_smart_invoice()
+    await callback.message.answer(link)
+    print("Invoice init")
+
+
 @router.callback_query(F.data == 'Main')
 async def others(callback: CallbackQuery):
-    await callback.answer('Вы в главном меню')
+    await callback.answer(lang.text_answers['main_menu_greetings'])
     await startup_user_dialog(callback)
 
 
@@ -34,14 +70,14 @@ async def user_db_check(message: Message):
 
 @router.callback_query(F.data == 'Others')
 async def others(callback: CallbackQuery):
-    await callback.answer('Раздел инструкций')
-    await callback.message.edit_text('Выберите свою платформу:', reply_markup=kb.others)
+    await callback.answer(lang.text_answers['instruction_greetings'])
+    await callback.message.edit_text(lang.text_answers['instruction_platform_choose'], reply_markup=kb.others)
 
 
 @router.callback_query(F.data == 'Android_Help')
 async def others(callback: CallbackQuery):
-    await callback.answer('Инструкция для Android')
-    await callback.message.edit_text(text=text_help, parse_mode='HTML', disable_web_page_preview=True,
+    await callback.answer(lang.text_answers['instruction_android'])
+    await callback.message.edit_text(text=ru.text_help, parse_mode='HTML', disable_web_page_preview=True,
                                      reply_markup=kb.others)
 
 
