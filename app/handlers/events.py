78de300e27c74028422ccf098577ac5978866d4c
SpicyCheeze <sinikovdmitriy@gmail.com>
2025-09-16 21:50:55 +0300

    userlist update

--- app/handlers/events.py
+++ app/handlers/events.py
@@ -4,6 +4,8 @@ import app.locale.lang_ru as ru
 from app.database.models import async_main
 from app.settings import bot, secrets
 from app.views import start_bot_msg, stop_bot_msg
+from io import BytesIO
+from aiogram.types import BufferedInputFile
 
 lang = eval(f"{secrets.get('language')}")
 crypto = secrets.get('crypto')
@@ -14,16 +16,16 @@ async def start_bot():
     await async_main()  # Создание таблиц БД при запуске
 
 
-
 async def userlist():
     all_users = await rq.get_users()
-    usrids = ""
+    i = 0
+    usrids = f"| № | Tg_id "
     for User in all_users:
-        if len(usrids) >= 3000:
-            return
-        else:
-            usrids = f"{usrids}\n{User.tg_id}"
-    await bot.send_message(chat_id=secrets.get('admin_id'), text=usrids)
+        usrids = f"{usrids}\n| {i} | {User.tg_id} |"
+        i = i + 1
+    file_bytes = usrids.encode('utf-8')
+    file = BufferedInputFile(file_bytes, filename='users.txt')
+    await bot.send_document(chat_id=secrets.get('admin_id') , document=file, caption='Here is your userlist')
 
 
 async def stop_bot():
@@ -32,11 +34,12 @@ async def stop_bot():
 
 async def main_menu(message, menu_type):
     if menu_type == "pro":
-        await message(lang.start_pro+lang.start_agreement, reply_markup=kb.main_pro, parse_mode="HTML")
+        await message(lang.start_pro + lang.start_agreement, reply_markup=kb.main_pro, parse_mode="HTML")
     elif menu_type == "free":
-        await message(lang.start_free+lang.start_agreement, reply_markup=kb.main_free, parse_mode="HTML")
+        await message(lang.start_free + lang.start_agreement, reply_markup=kb.main_free, parse_mode="HTML")
     else:
-        await message(lang.start_base+lang.start_new+lang.start_agreement, reply_markup=kb.main_new, parse_mode="HTML")
+        await message(lang.start_base + lang.start_new + lang.start_agreement, reply_markup=kb.main_new,
+                      parse_mode="HTML")
 
 
 async def main_call(message, menu_type):
