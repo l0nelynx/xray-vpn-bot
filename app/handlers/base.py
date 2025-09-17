ca04a9d5781712b094a168430e6c19a7ca359359
l0nelynx <113856580+l0nelynx@users.noreply.github.com>
2025-09-17 11:22:26 +0300

    Update base.py

--- app/handlers/base.py
+++ app/handlers/base.py
@@ -83,7 +83,16 @@ async def broadcast_make(message: Message):
     await message.answer('Making a broadcast')
     #command_args: str = message.get_args()
     #print(command_args)
-    await admin_broadcast(message)
+    await admin_broadcast(message,0)
+
+
+@router.message(Command("broadcast1"), F.from_user.id == secrets.get('admin_id'))  #
+async def broadcast_make(message: Message):
+    await message.answer('Making a broadcast')
+    #command_args: str = message.get_args()
+    #print(command_args)
+    await admin_broadcast(message,1)
+
 
 broadcast_active = False
 
