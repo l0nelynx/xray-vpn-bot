9a3d3d35e7e4df1ee60823f8f3beffd19beca5be
l0nelynx <113856580+l0nelynx@users.noreply.github.com>
2025-11-13 15:21:38 +0300

    Update templates.py

--- app/marzban/templates.py
+++ app/marzban/templates.py
@@ -20,3 +20,26 @@ vless_template = {
     "on_hold_timeout": "2023-11-03T20:30:00",
     "on_hold_expire_duration": 0
 }
+pro_template = {
+    "username": "user1234",
+    "proxies": {
+        "vless": {
+            "id": "35e4e39c-7d5c-4f4b-8b71-558e4f37ff53",
+            "flow": "xtls-rprx-vision"
+        }
+    },
+    "inbounds": {
+        "vless": [
+            "VLESS+TCP+REALITY+9696",
+            "VLESS+XHTTP+REALITY+9797",
+            "VLESS+WS+TLS+9992"
+        ]
+    },
+    "expire": 0,
+    "data_limit": 0,
+    "data_limit_reset_strategy": "no_reset",
+    "status": "active",
+    "note": "",
+    "on_hold_timeout": "2023-11-03T20:30:00",
+    "on_hold_expire_duration": 0
+}
